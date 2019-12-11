0a4d041dd2d779e176cf4c19272c104648172c98
jig10 <jigodoyrubin@gmail.com>
2019-12-11 01:21:31 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [todo, setTodo] = useState([
+		{
+			text: "shower",
+			isComplete: false
+		},
+		{
+			text: "eat",
+			isComplete: false
+		},
+		{
+			text: "play",
+			isComplete: false
+		}
+	]);
+	const [temp, setTemp] = useState();
+
+	const completeTodo = index => {
+		console.log("Hello");
+	};
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<input
+				onChange={event => setTemp(event.target.value)}
+				onKeyPress={e => {
+					if (e.key === "Enter") {
+						setTodo([...todo, { text: temp, isComplete: false }]);
+						e.target.value = "";
+					}
+				}}
+			/>
+			{todo.map((item, index) => {
+				return (
+					<div key={index}>
+						{item.text}
+						<button onClick={() => completeTodo(index)}>x</button>
+					</div>
+				);
+			})}
 		</div>
 	);
 }
